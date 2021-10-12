07f50c5e6f6d2800ed35abae02c09735617d2e28
Doug Wile <wile.doug@gmail.com>
2021-10-12 18:13:35 -0500

    fix: make the server runnable by updating the package.json

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.port || 5001;
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello from express!</h1>");
