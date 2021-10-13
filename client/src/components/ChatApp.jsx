e7bb18d9eef723690ab673f841db15b720ba4c78
Eric Treadway <eric.s.treadway@gmail.com>
2021-10-12 19:02:07 -0500

    slight port change

--- client/src/components/ChatApp.jsx
+++ client/src/components/ChatApp.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("http://localhost:5001");
 
 export const ChatApp = () => {
   const [messageList, setMessageList] = useState([
